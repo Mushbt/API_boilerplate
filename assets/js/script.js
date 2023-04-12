88860f70e246e597055e210cabf24584b73a5163
Mushbt <mustafa_habet@hotmail.com>
2023-04-12 11:45:14 +0000

    TEST

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-const API_KEY = "https://ci-jshint.herokuapp.com/api?api_key=EMhRT8qubGNPBG2civ5t3q7Xitw";
+const API_KEY = "EMhRT8qubGNPBG2civ5t3q7Xitw";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultsModal = new bootstrap.Modal(document.getElementById("resultModal"));
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -12,6 +13,7 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        console.log(data.expiry);
     }
+
 }
